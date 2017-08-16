cc92abbfbf57644051e395d8ebf3fd89b176c4c0
github username <github email address>
2017-08-16 10:18:31 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :students, only: [:index, :new, :create, :edit, :show, :update]
+  resources :school_classes
 end
